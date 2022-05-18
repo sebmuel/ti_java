a81c59d3725a2c3fca2bb38d78da96f1c094a6fc
sebmuel <mueller.sebastian117@gmail.com>
2022-05-18 15:09:25 +0200

    Finetuned Code

--- src/validBrackets/BracketChecker.java
+++ src/validBrackets/BracketChecker.java
@@ -1,14 +1,18 @@
 package validBrackets;
 
+import java.util.HashMap;
 import java.util.Stack;
 
 public class BracketChecker {
     public static boolean check(String str){
         Stack<Character> stack = new Stack<>();
-
         for (Character ch :  str.toCharArray()) {
             if (ch == '(' || ch == '{' || ch == '['){
                 stack.push(ch);
+            }
+            // if current char is a closing bracket of any kind and the stack is empty we can return false
+            if (stack.isEmpty() && (ch == ']' || ch == '}' || ch == ')')){
+                return false;
             }
             char topOfStack;
             switch(ch){
--- src/validBrackets/Main.java
+++ src/validBrackets/Main.java
@@ -2,10 +2,8 @@ package validBrackets;
 
 
 public class Main {
-
     public static void main(String[] args){
         boolean string1 = BracketChecker.check("(Hallodu123[was{ge}asdasd]Adasd)");
         System.out.println(string1);
     }
-
 }
